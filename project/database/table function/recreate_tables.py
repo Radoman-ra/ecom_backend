7b93f39f8947484e92d3a7cad031b07a301ceb83
rad <rmaksim886@gmail.com>
2024-08-21 16:21:15 +0200

    bug fix

--- project/database/table function/recreate_tables.py
+++ project/database/table function/recreate_tables.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+)
+
 from database.tables import User, Supplier, Category, Product, Order
 from database.database import Base, root_engine
 
