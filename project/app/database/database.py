8a180aaab062ca16e3b797c5fddf4b7fe8c30be2
Radoman-ra <rmaksim886@gmail.com>
2024-10-14 22:25:19 +0200

    bug fix

--- project/app/database/database.py
+++ project/app/database/database.py
@@ -8,19 +8,12 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-MYSQL_ROOT_PASSWORD= "qwerty"
-MYSQL_DATABASE= "ecommerce_product_api"
-MYSQL_USER= "mysql"
-MYSQL_PASSWORD= "q1w2e3e3w2q1"
-MYSQL_HOST= "db"
-MYSQL_PORT= 3306
-
-# MYSQL_USER = os.getenv("MYSQL_USER")
-# MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-# MYSQL_HOST = os.getenv("MYSQL_HOST", "db")
-# MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
-# MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
-# MYSQL_ROOT_PASSWORD = os.getenv("MYSQL_ROOT_PASSWORD")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_HOST = os.getenv("MYSQL_HOST", "db")
+MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+MYSQL_ROOT_PASSWORD = os.getenv("MYSQL_ROOT_PASSWORD")
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
 print(SQLALCHEMY_DATABASE_URL)
