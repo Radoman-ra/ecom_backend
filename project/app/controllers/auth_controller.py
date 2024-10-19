1b498d69ea550fe9a007809b6fdbf55798a0e649
Radoman-ra <rmaksim886@gmail.com>
2024-10-19 17:06:13 +0200

    Refactor auth_controller.py to update Google OAuth authorize URL

--- project/app/controllers/auth_controller.py
+++ project/app/controllers/auth_controller.py
@@ -28,7 +28,7 @@ oauth.register(
     name='google',
     client_id=os.getenv('GOOGLE_CLIENT_ID'),
     client_secret=os.getenv('GOOGLE_CLIENT_SECRET'),
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_url='https://accounts.google.com/o/oauth2/v2/auth',
     authorize_params=None,
     access_token_url='https://accounts.google.com/o/oauth2/token',
     access_token_params=None,
