d2b254c02637e1ce14aaf52b5f96645bf087e2e6
Radoman-ra <rmaksim886@gmail.com>
2024-10-19 16:12:29 +0200

    fix: google oauth

--- project/app/routers/auth.py
+++ project/app/routers/auth.py
@@ -16,11 +16,11 @@ router = APIRouter(prefix="/api/auth", tags=["auth"])
 
 @router.get("/login/google")
 async def login_with_google(request: Request):
-    return login_via_google(request)
+    return await login_via_google(request)
 
 @router.get("/google/callback", response_model=TokenResponse)
 async def google_callback(request: Request, db: Session = Depends(get_db)):
-    return handle_google_callback(request, db)
+    return await  handle_google_callback(request, db)
 
 @router.post("/login", response_model=TokenResponse)
 async def login(
@@ -28,7 +28,7 @@ async def login(
     db: Session = Depends(get_db),
     response: Response = Response(),
 ):
-    return login_user(form_data, db, response)
+    return await  login_user(form_data, db, response)
 
 
 @router.post("/refresh", response_model=TokenResponse)
@@ -37,7 +37,7 @@ async def refresh_token_auth(
     refresh_token: str,
     db: Session = Depends(get_db),
 ):
-    return refresh_access_token(response, refresh_token, db)
+    return await  refresh_access_token(response, refresh_token, db)
 
 
 @router.post(
@@ -46,9 +46,9 @@ async def refresh_token_auth(
     response_description="Successfully logged out",
 )
 async def logout(response: Response, authorization: str = Header(None)):
-    return logout_user(response, authorization)
+    return await  logout_user(response, authorization)
 
 
 @router.post("/register", response_model=UserCreate)
 async def register_user(user_data: UserCreate, db: Session = Depends(get_db)):
-    return register_new_user(user_data, db)
+    return await  register_new_user(user_data, db)
